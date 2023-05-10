838d3616400a5f03f6fe9e2a016fd58696210952
Jorge Marroquin <90352955+JRoca00@users.noreply.github.com>
2023-05-10 07:48:08 -0600

    Add files via upload

---
+++ custom_puma/__manifest__.py
@@ -0,0 +1,19 @@
+{
+
+    'name': 'Recibir Mercaderia',
+    'version': '15.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Recibir mercaderia solo ecnargados de tienda',
+    'license': 'AGPL-3',
+    'author': 'Panela Bits',
+    'maintainer': 'Panela Bits',
+    'website': 'https://panelabits.com',
+    'depends': ['stock'],
+    'demo': [],
+    'data': [
+        'views/stock_confirm.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
