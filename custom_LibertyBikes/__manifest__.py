771580739de90845ebe91c5e9166426c92ee8729
Jorge Marroquin <90352955+JRoca00@users.noreply.github.com>
2023-06-29 16:18:05 -0600

    Add files via upload

---
+++ custom_LibertyBikes/__manifest__.py
@@ -0,0 +1,20 @@
+{
+
+    'name': 'Reportes Recursos Humanos',
+    'version': '15.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Reportes Recursos Humanos',
+    'license': 'AGPL-3',
+    'author': 'Panela Bits',
+    'maintainer': 'Panela Bits',
+    'website': 'https://panelabits.com',
+    'depends': ['hr'],
+    'demo': [],
+    'data': [
+        'report/hr_employee_report.xml',
+        'report/hr_employee.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
