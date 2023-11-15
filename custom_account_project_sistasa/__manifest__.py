7c10516a8519129e8b2e6dce01d595163dc8d4ba
Jorge Marroquin <90352955+JRoca00@users.noreply.github.com>
2023-11-15 10:59:03 -0600

    Add files via upload

---
+++ custom_account_project_sistasa/__manifest__.py
@@ -0,0 +1,20 @@
+{
+
+    'name': 'Facturas en proyectos',
+    'version': '16.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Vincula facturas a proyectos',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'author': 'PanelaBits',
+    'maintainer': 'PanelaBits',
+    'website': 'panelabits.com',
+    'depends': ['account'],
+    'demo': [],
+    'data': [
+        'views/AccountMove.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
