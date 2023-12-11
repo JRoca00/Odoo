e6d3302356375304236a72a4691bcb99da0de7a8
Jorge Marroquin <90352955+JRoca00@users.noreply.github.com>
2023-12-11 11:08:12 -0600

    Add files via upload

---
+++ custom_contact/__manifest__.py
@@ -0,0 +1,21 @@
+{
+
+    'name': 'Contact Center',
+    'version': '13.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Registro de casos',
+    'license': 'AGPL-3',
+    'author': 'Panela Bits',
+    'maintainer': 'Panela Bits',
+    'website': 'https://panelabits.com',
+    'depends': ['stock'],
+    'demo': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/contact_center.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
